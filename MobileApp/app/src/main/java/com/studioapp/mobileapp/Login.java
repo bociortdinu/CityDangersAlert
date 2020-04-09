181d7b6a75d8eff98e8e9825b9fba65b46ca4b5d
bociortdinu <61885165+bociortdinu@users.noreply.github.com>
2020-04-09 23:14:11 +0300

    Main activity; LoginActivity partial spre deloc;

---
+++ MobileApp/app/src/main/java/com/studioapp/mobileapp/Login.java
@@ -0,0 +1,36 @@
+package com.studioapp.mobileapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = (Button)findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMeniu();
+            }
+        });
+    }
+
+    private void openMeniu(){
+        Intent intent = new Intent(this , Meniu.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+}
--- MobileApp/app/src/main/java/com/studioapp/mobileapp/MainActivity.java
+++ MobileApp/app/src/main/java/com/studioapp/mobileapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.studioapp.mobileapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        try {
+
+            Thread.sleep(5000);
+            Intent intetn = new Intent(this, Login.class);
+            startActivity(intetn);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
+
+
+
+
