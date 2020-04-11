0f312595dfc90d363b819eed3a535e9847e3c301
bociortdinu <61885165+bociortdinu@users.noreply.github.com>
2020-04-11 14:55:40 +0300

    Meniu Switch

--- MobileApp/app/src/main/java/com/studioapp/mobileapp/Meniu.java
+++ MobileApp/app/src/main/java/com/studioapp/mobileapp/Meniu.java
@@ -1,13 +1,56 @@
 package com.studioapp.mobileapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class Meniu extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meniu);
+
+        bottomNav = findViewById(R.id.bottom_navigation);
+
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new ProfilFragment()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_profil:
+                            selectedFragment = new ProfilFragment();
+                            break;
+                        case R.id.nav_monitorizare:
+                            selectedFragment = new MonitorizareFragment();
+                            break;
+                        case R.id.nav_activitati:
+                            selectedFragment = new ActivitatiFragment();
+                            break;
+                        case R.id.nav_recomandari:
+                            selectedFragment = new RecomandariFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
 }
