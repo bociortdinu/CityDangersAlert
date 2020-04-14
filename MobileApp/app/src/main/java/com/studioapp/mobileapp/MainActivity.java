1fbcca33c9272c9702da823ef22a43cc62233410
cameliaburlacu98 <51081051+cameliaburlacu98@users.noreply.github.com>
2020-04-14 03:09:06 +0300

    progressbar

--- MobileApp/app/src/main/java/com/studioapp/mobileapp/MainActivity.java
+++ MobileApp/app/src/main/java/com/studioapp/mobileapp/MainActivity.java
@@ -1,45 +1,33 @@
 package com.studioapp.mobileapp;
 
 import android.content.Intent;
-import android.os.Handler;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable(){
-                @Override
-                public void run(){
-                    startLogin();
-                }
-            }, 3000);
-        }
-        finally {
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable(){
-                @Override
-                public void run(){
-                    finish();
-                }
-            }, 3000);
-        }
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        progressBar=findViewById(R.id.id_progressBar);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+        progressAnimation();
 
     }
-
-
-    public void startLogin()
-    {
-        Intent intent = new Intent(this, Login.class);
-        startActivity(intent);
-
+    public void progressAnimation() {
+        ProgressBarAnimation amin = new ProgressBarAnimation(this, progressBar, 0f, 100f);
+        amin.setDuration(8000);
+        progressBar.setAnimation(amin);
     }
-
 }
 
 
